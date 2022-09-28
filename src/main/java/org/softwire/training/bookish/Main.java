bd92b2fb43ce17564ede9f4d31f5e197ece3f15f
Kit Bolourchi <kit.bolourchi@aexp.com>
2022-09-28 11:19:31 +0100

    Commit incase of error

--- src/main/java/org/softwire/training/bookish/Main.java
+++ src/main/java/org/softwire/training/bookish/Main.java
@@ -2,9 +2,8 @@ package org.softwire.training.bookish;
 
 import org.jdbi.v3.core.Jdbi;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.List;
 
 
 public class Main {
@@ -12,8 +11,8 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         String hostname = "localhost";
         String database = "bookish";
-        String user = "bookish";
-        String password = "bookish";
+        String user = "root";
+        String password = "password";
         String connectionString = "jdbc:mysql://" + hostname + "/" + database + "?user=" + user + "&password=" + password + "&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=GMT&useSSL=false";
 
         jdbcMethod(connectionString);
@@ -26,10 +25,17 @@ public class Main {
         // TODO: print out the details of all the books (using JDBC)
         // See this page for details: https://docs.oracle.com/javase/tutorial/jdbc/basics/processingsqlstatements.html
 
-        Connection connection = DriverManager.getConnection(connectionString);
-
-
-
+        try (Connection connection = DriverManager.getConnection(connectionString)) {
+            Statement st = connection.createStatement();
+            String query = ("SELECT Title FROM Books;");
+            ResultSet rs = st.executeQuery(query);
+            while (rs.next()) {
+                String Book = rs.getString("Title");
+                System.out.println(Book);
+            }
+        } catch (SQLException e) {
+            System.out.println("Cannot connect to database");
+        }
     }
 
     private static void jdbiMethod(String connectionString) {
@@ -41,7 +47,12 @@ public class Main {
 
         Jdbi jdbi = Jdbi.create(connectionString);
 
-
-
+        List<String> books = jdbi.withHandle(handle ->
+                handle.createQuery("SELECT Title From Books;")
+                        .mapTo(String.class)
+                        .list());
+        for (String book: books) {
+            System.out.println(book);
+        }
     }
 }
